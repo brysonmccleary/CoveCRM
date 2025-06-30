e1109023c5c0c2e0d798d6ae9961db15dbe0f83d
Bryson Mccleary <brysonmccleary@Brysons-MacBook-Pro.local>
2025-06-30 12:04:32 -0700

    Add MongoDB connection file

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,25 +1,23 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
-
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI as string;
 const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
 
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your MongoDB URI to .env.local");
+}
+
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable to preserve value across module reloads
   if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri!, options);
+    client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  // In production mode, no global variable
-  client = new MongoClient(uri!, options);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
