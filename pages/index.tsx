9cdb48234d3811e9f26b118c99376d55a1767343
Bryson Mccleary <brysonmccleary@Brysons-MacBook-Pro.local>
2025-06-30 11:27:57 -0700

    Re-add pages directory with basic index page

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>CoveCRM is working!</h1>
+      <p>You can now start adding your pages and components back one by one.</p>
+    </div>
+  );
+}
+
