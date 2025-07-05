4dd2ff74568e364f977867a52653d779b740c017
Bryson Mccleary <brysonmccleary@Brysons-MacBook-Pro.local>
2025-07-04 23:24:51 -0700

    Actually remove old fieldMappings import from LeadImportPanel

--- LeadImportPanel.tsx
+++ LeadImportPanel.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { matchColumnToField, STANDARD_FIELDS } from "../utils/fieldMappings";
 import { saveMappingToLocal, getSavedMappings } from "../utils/mappingStorage";
 
 const LeadImportPanel = ({ csvData }) => {
