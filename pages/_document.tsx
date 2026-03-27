ebae6c9560d8422646377fa46569f4e9a89a12aa
Bryson Mccleary <Bryson.mccleary1@gmail.com>
2026-03-27 13:56:29 -0700

    Add Meta domain verification tag to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ export default function Document() {
         {/* ✅ Updated brand name */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta name="facebook-domain-verification" content="hwc2mtikjkyvgdmeja5be0vxjucafo" />
       </Head>
       <body>
         <Main />
